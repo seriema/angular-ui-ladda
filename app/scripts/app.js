97de2bcb562c054a8d46819cfe20f97ac6797740
John-Philip Johansson <seriema@gmail.com>
2014-06-16 20:08:13 +0200

    chore(main): first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularUiLaddaApp
+ * @description
+ * # angularUiLaddaApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularUiLaddaApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularUiLaddaApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularUiLaddaApp
+ */
+angular.module('angularUiLaddaApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
